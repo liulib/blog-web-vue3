f592230054aba10b3ad070fec014cbc4c1d83e09
liulib <liulibzz@163.com>
2021-11-08 11:27:10 +0800

    fix：修改环境变量，cdn引入依赖

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ import '@/assets/styles/reset.css';
 
 import '@/assets/icons/iconfont.css';
 
+import antDesignVue from 'ant-design-vue';
+import VMdEditor from '@kangc/v-md-editor';
+
 const app = createApp(App);
 
 // 注册全局常用的ant-design-vue组件
@@ -17,6 +20,8 @@ setupAntd(app);
 setupRouter(app);
 
 setupMarkDown(app);
+
+app.use(antDesignVue).use(VMdEditor);
 
 // 路由准备就绪后挂载APP实例
 app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,37 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
+import { visualizer } from 'rollup-plugin-visualizer';
+
+import externalGlobals from 'rollup-plugin-external-globals';
+
+import { loadEnv } from 'vite';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    server: {
-        host: '127.0.0.1',
-        port: 9001
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
+export default ({ mode }) => {
+    return defineConfig({
+        plugins: [vue()],
+        server: {
+            host: '127.0.0.1',
+            port: Number(loadEnv(mode, process.cwd()).VITE_APP_PORT)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
+        },
+        build: {
+            rollupOptions: {
+                external: ['vue', 'ant-design-vue', '@kangc/v-md-editor'],
+                plugins: [
+                    visualizer(),
+                    externalGlobals({
+                        vue: 'Vue',
+                        'ant-design-vue': 'AntDesignVue',
+                        '@kangc/v-md-editor': 'VMdEditor'
+                    })
+                ]
+            }
         }
-    }
-});
+    });
+};
