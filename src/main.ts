14d60c264e947d06c169e5f434caa3a8c9ba792e
liulib <liulibzz@163.com>
2021-11-09 16:20:46 +0800

    fix：cdn打包失败（暂时无法成功，等待替换markdown插件），修复回原来版本

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,17 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import { setupRouter } from '@/router/index';
-
-import { setupAntd, setupMarkDown } from '@/plugins';
+import { setupAntd, setupMarkDown } from '@/plugins/index';
 
 import '@/assets/styles/reset.css';
 
 import '@/assets/icons/iconfont.css';
 
-import antDesignVue from 'ant-design-vue';
-import VMdEditor from '@kangc/v-md-editor';
-
 const app = createApp(App);
 
-// 注册全局常用的ant-design-vue组件
-setupAntd(app);
-
 setupRouter(app);
-
+setupAntd(app);
 setupMarkDown(app);
-
-app.use(antDesignVue).use(VMdEditor);
 
 // 路由准备就绪后挂载APP实例
 app.mount('#app');
